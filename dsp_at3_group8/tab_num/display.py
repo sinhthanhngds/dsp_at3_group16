34fc1d6403b782a27daa3fa498b7e5fff1f01b22
Hellostar-0001 <126056021+henryWorkAcc@users.noreply.github.com>
2023-10-24 22:21:40 +1100

    Sample Frontend
    Just a frontend display without any actual data

--- dsp_at3_group8/tab_num/display.py
+++ dsp_at3_group8/tab_num/display.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 from tab_num.logics import NumericColumn
 
@@ -27,4 +28,53 @@ def display_tab_num_content(file_path=None, df=None):
     -> None
 
     """
+    instance = NumericColumn()
+    data_columns = ['random string', 'random string1', 'random string 2']
+
+    # st.title("FX Converter")
+
+    select_column = st.selectbox("Which numeric column do you want to explore", data_columns)
+
+    # Based on select_column, generate below data
+    # 
+    # NumericColumn.set_data(select_column)
+    # num = NumericColumn.get_summary(<Get Numeric Column Table>)
+    num = {
+        '': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        'Description': [
+            "Number of Unique Values",
+            "Number of Missing Values",
+            "Number of Ocurrence of 0 Value",
+            "Number of Negative Values",
+            "The Average Value",
+            "The Standard Deviation Value",
+            "The Minimum Value",
+            "The Maximum Value",
+            "The Median Value"
+            ],
+        'Value': [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+
+
+    num_column = pd.DataFrame(num)
+
+    st.write("Numeric Column")
+    st.table(num_column)
     
+    # NumericColumn.set_histogram()
+    # st.altair_chart(instance.histogram)
+
+    #num = NumericColumn.get_summary(<Get Most Frequent Values Table>) 
+    freq = {
+        '': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'value': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'occ': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'percentage': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+
+    freq_Data = pd.DataFrame(freq)
+
+    st.write("Most Frequent Values")
+    st.table(freq_Data)
+
+# display_tab_num_content()
